38e617ee693a94b6213acfff77cfaef80bbb7bda
axandrews <38111017+axandrews@users.noreply.github.com>
2018-04-25 18:23:54 -0700

    Initial untested attempt at a resource select screen

---
+++ ResourcesSelectScreen.java
@@ -0,0 +1,106 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ResourcesSelectScreen extends JLayeredPane{
+
+	private JLabel backgroundPanel;
+	private JPanel buttonPanel = new JPanel();
+	private Font font = new Font(Font.MONOSPACED, Font.PLAIN, 20);
+	private JButton [] playerResourceButtons;
+	private JTextField scoreDisplay;
+	
+	private Player currentPlayer;
+	private int score;
+	private int [] multipliers;
+	private boolean resourceSelected;
+	private int resource; 
+	
+	public RollScreen(Player currentPlayer) {
+		this.currentPlayer = currentPlayer;
+		this.score = currentPlayer.getScore();
+		this.multipliers = currentPlayer.getMultipliers(); 
+		
+		
+		backgroundPanel = new JLabel(new ImageIcon("backgroundPlanet" + currentPlayer.getLocation() + ".png"));
+		backgroundPanel.setOpaque(true);
+		backgroundPanel.setBounds(0,0,800,600);
+		add(backgroundPanel, new Integer(0));
+		initPlayerResourceButtons();
+		
+		buttonPanel.setOpaque(false);
+		buttonPanel.setBounds(600,50,160,90);
+		add(buttonPanel, new Integer(2));
+		
+	}
+
+		/**
+	 * initializes and fills array of resource select buttons as well as score  display
+	 */
+	private void initPlayerResourceButtons() {
+		playerResourceButtons = new JButton[5];
+		
+		scoreDisplay = new JTextField(10);
+		scoreDisplay.setText("Score: " + score);
+		scoreDisplay.setBackground(Color.BLACK);
+		scoreDisplay.setForeground(Color.yellow);
+		scoreDisplay.setFont(font);
+		scoreDisplay.setOpaque(true);
+		scoreDisplay.setBounds(0, 50, 400, 200);
+		scoreDisplay.setEditable(false);
+		
+		
+		playerResourceButtons[0] = new JButton("Parts: " + score + " X " + multipliers[0] + " = " + score*multipliers[0]);
+		playerResourceButtons[1] = new JButton("Fuel: " + score + " X " + multipliers[1] + " = " + score*multipliers[1]);
+		playerResourceButtons[2] = new JButton("Money: " + score + " X " + multipliers[2] + " = " + score*multipliers[2]);
+		playerResourceButtons[3] = new JButton("Necessities: " + score + " X " + multipliers[3] + " = " + score*multipliers[3]);
+		playerResourceButtons[4] = new JButton("Titanium: " + score + " X " + multipliers[4] + " = " + score*multipliers[4]);
+		
+		
+		playerResourceButtons[0].setBounds(0, 150, 400, 200);
+		playerResourceButtons[1].setBounds(400, 150, 400, 200);
+		playerResourceButtons[2].setBounds(0, 275, 400, 200);
+		playerResourceButtons[3].setBounds(400, 275, 400, 200);
+		playerResourceButtons[4].setBounds(0, 400, 400, 200);
+		
+		ResourceAction partsAction = new ResourceAction(0);
+		ResourceAction fuelAction = new ResourceAction(1);
+		ResourceAction moneyAction = new ResourceAction(2);
+		ResourceAction necessitiesAction = new ResourceAction(3);
+		ResourceAction titaniumAction = new ResourceAction(4);
+		
+		playerResourceButtons[0].addActionListener(partsAction);
+		playerResourceButtons[1].addActionListener(partsAction);
+		playerResourceButtons[2].addActionListener(partsAction);
+		playerResourceButtons[3].addActionListener(partsAction);
+		playerResourceButtons[4].addActionListener(partsAction);
+		
+		for (int i = 0; i < 5; i++) {
+			buttonPanel.add(playerResourceButtons[i]);
+		}
+	}
+	private class ResourceAction implements ActionListener {
+		private int aResource; 
+		
+		public ResourceAction(int resource) {
+			aResource = resource;
+		}
+		
+		public void actionPerformed(ActionEvent event) 
+		{
+			resourceSelected = true;
+			resource = aResource;
+		}
+	}
+	
+	public boolean isResourceSelected() {
+		return resourceSelected;
+	}
+	
+	public int getResource() {
+		return resource;
+	}
+	
+
+}
